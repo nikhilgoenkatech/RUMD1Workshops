8980ae4cd729c58cb057b1c02594a848e62c1075
nikhilgoenkatech <52188007+nikhilgoenkatech@users.noreply.github.com>
2021-07-15 20:17:58 +1000

    Update install-rum-prerequisites.sh

--- install-rum-prerequisites.sh
+++ install-rum-prerequisites.sh
@@ -31,11 +31,11 @@ downloadStandAloneSetup() {
     pip3 uninstall Django -y
     pip3 install Django==2.2.10
 
-    cp $RETAILAPP_DIR/nginx.default /etc/nginx/sites-enabled/default
-    pip3 install -r $RETAILAPP_DIR/requirements.txt
+    cp /home/ubuntu/ecommerce/nginx.default /etc/nginx/sites-enabled/default
+    pip3 install -r /home/ubuntu/ecommerce/requirements.txt
     
     python3.6 manage.py collectstatic --noinput
-    cp -r $RETAILAPP_DIR/src/static /static/
+    cp -r /home/ubuntu/ecommerce/src/static /static/
     printInfoSection "Installed Standalone pre-requisites"
 
     printInfo "Install Docker"
